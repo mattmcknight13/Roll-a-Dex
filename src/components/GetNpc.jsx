7329b0f344844860e056df189a6d3137e3df6f58
Matthew McKnight <matthewmcknight@matthews-mbp.home>
2020-09-09 16:32:56 -0500

    can get only one side of ternary need to find proper way to get them to swap then mvp is met, all will be left is styling

--- src/components/GetNpc.jsx
+++ src/components/GetNpc.jsx
@@ -7,23 +7,32 @@ const GetNpc = (props) => {
   // destructure props object
   const { npcKey, npc, getRandomNpc, getStat, npcInfo, setNpcInfo } = props;
 
+  let npcInfoKeys = Object.keys(npcInfo);
+  let npcInfoValues = Object.values(npcInfo);
+
   return (
     <div className="randomnpc">
       <>
         {/* have button trigger api call on click */}
         <RandomButton getRandomNpc={getRandomNpc} getStat={getStat} />
         {/* map through selected object to give key values to be classname, and assign values to approriate h2 tag */}
-        {!npcInfo
-          ? npc.map((n, idx) => (
-              <h2 className={npcKey[idx]} key={idx}>
-                {npcKey[idx]}: {n}
-              </h2>
-            ))
-          : npcInfo.map((n, idx) => ( //looking into object.entries but ran into html element issues i think i'm on right track
-              <h2 className={npcInfo[idx]} key={idx}>
-                {npcInfo[idx]}: {n}
-              </h2>
-            ))}
+
+        {npcInfo ? (
+          npc.map((n, idx) => (
+            <h2 className={npcKey[idx]} key={idx}>
+              {npcKey[idx]}: {n}
+            </h2>
+          ))
+        ) : (
+          <div>
+            <h2 className={npcInfoKeys[0]}>Name: {npcInfoValues[0]}</h2>
+            <h2 className={npcInfoKeys[1]}>Race: {npcInfoValues[1]}</h2>
+            <h2 className={npcInfoKeys[2]}>Sex: {npcInfoValues[2]}</h2>
+            <h2 className={npcInfoKeys[3]}>Job: {npcInfoValues[3]}</h2>
+            <h2 className={npcInfoKeys[4]}>Description: {npcInfoValues[4]}</h2>
+            <h2 className={npcInfoKeys[5]}>Story Hook: {npcInfoValues[5]}</h2>
+          </div>
+        )}
       </>
     </div>
   );
