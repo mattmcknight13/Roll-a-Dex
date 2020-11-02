82d0eeefb6d8bf09e73f33073773a8664b8cfdbf
Matthew McKnight <matthewmcknight@matthews-mbp.home>
2020-11-02 13:29:42 -0600

    testing link changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import { Link} from "react-router-dom";
-import "./Nav.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
 
 function Nav() {
-
   const pageButtonStyle = {
     color: "black",
     border: "solid 1px rgb(107,107,107)",
     background: "rgb(237,237,237)",
     textDecoration: "none",
-    justifyContent: "spaceEvenly"
+    justifyContent: "spaceEvenly",
   };
 
   return (
     <div>
-       <nav className="nav">
-            <Link style={pageButtonStyle} to="/">
-              Create NPC
-            </Link>
-            <Link style={pageButtonStyle} to="/sheet">
-              Random Npc
-            </Link>
-          </nav>
+      <nav className="nav">
+        <Link style={pageButtonStyle} to="/">
+          Create NPC
+        </Link>
+        <Link style={pageButtonStyle} to="/sheet">
+          Random Npc
+        </Link>
+      </nav>
     </div>
   );
 }
